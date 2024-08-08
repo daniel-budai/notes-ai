a42662df0681d86a628315c2cc63b272161e91fc
daniel-budai <danielbudai@tutanota.com>
2024-08-08 11:34:29 +0200

    textarea and form

--- src/components/ui/AddNoteDialog.tsx
+++ src/components/ui/AddNoteDialog.tsx
@@ -1,8 +1,17 @@
 import { createNoteSchema, CreateNoteSchema } from "@/lib/validation/note";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Dialog, DialogContent, DialogHeader } from "./dialog";
-import { Form } from "./form";
+import { Dialog, DialogContent, DialogFooter, DialogHeader } from "./dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
+import { Input } from "./input";
+import { Textarea } from "./textarea";
 
 interface AddNoteDialogProps {
   open: boolean;
@@ -25,7 +34,37 @@ export default function AddNoteDialog({ open, setOpen }: AddNoteDialogProps) {
           <DialogHeader>Add Note</DialogHeader>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={}></form>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Note title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                  {/* //show the error message from schema */}
+                </FormItem>
+              )}
+            />{" "}
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note content</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="content" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                  {/* //show the error message from schema */}
+                </FormItem>
+              )}
+            />
+            <DialogFooter></DialogFooter>
+          </form>
         </Form>
       </DialogContent>
     </Dialog>
