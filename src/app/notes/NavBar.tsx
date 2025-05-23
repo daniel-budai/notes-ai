2ad4ab469d6e9173e37df57b2a186cb5c1f942e1
daniel-budai <danielbudai@tutanota.com>
2025-05-23 22:08:01 +0200

    Ai components, darkmode colors, route for ai

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -11,6 +11,7 @@ import AddNoteDialog from "@/components/ui/AddEditNoteDialog";
 import ThemeToggleButton from "@/components/ThemeToggleButton";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
+import AIChatButton from "@/components/AiChatButton";
 
 export default function NavBar() {
   const { theme } = useTheme();
@@ -39,6 +40,7 @@ export default function NavBar() {
               <Plus size={20} className="mr-2" />
               Add note
             </Button>
+            <AIChatButton />
           </div>
         </div>
       </div>
